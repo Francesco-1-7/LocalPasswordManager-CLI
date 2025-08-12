d2a18fd930a96ce44d942f5e0601274ea75e011c
Francesco <francescopanarese96@gmail.com>
2025-08-12 02:39:22 +0200

    update

--- src/localpwd/corelogic.py
+++ src/localpwd/corelogic.py
@@ -1,20 +1,31 @@
-# Iniziamo a scrivere la logica del password manager
+import json
+
+def aggiungipassword():
+    with open("passwords.json", "r", encoding="utf-8") as file:
+        dati=json.load(file)
+        
+    sito=input("Inserisci il nome del sito: ")
+    username=input("Inserisci username/email da salvare: ")
+    password=input("Inserisci la password da salvare: ")
+
+    dati.append({
+        "sito": sito,
+        "username": username,
+        "password": password
+    })
+
+    with open("passwords.json", "w", encoding="utf-8") as file:
+        json.dump(dati, file, indent=4, ensure_ascii=False)
+
+    print("Password salvata con successo")
 
 def menu():
     while True:
-        print("\n===Localpwd Password Manager")
+        print("\n===Localpwd Password Manager===")
+        
         print("1. Aggiungi una nuova password")
         print("2. Visualizza le password salvate")
         print("3. Esci da Localpwd")
-
         scelta=input("Seleziona un'opzione (1-3): ").strip()
-
         if scelta == "1":
-            print("Funzione aggiunta password (da implementare)")
-        elif scelta == "2":
-            print("Funzione visualizza password (da implementare)")
-        elif scelta == "3":
-            print("Uscita in corso...")
-            break
-        else:
-            print("Opzione non valida. Riprova.")
+            aggiungipassword()
