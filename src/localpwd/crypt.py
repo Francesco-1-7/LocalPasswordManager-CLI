86cba8703da0c26d90c1845cbf1e6821aa7011fa
Francesco <francescopanarese96@gmail.com>
2025-08-24 01:07:02 +0200

    update

--- src/localpwd/crypt.py
+++ src/localpwd/crypt.py
@@ -3,26 +3,25 @@ import base64
 import hashlib
 from cryptography.fernet import Fernet
 
-def derive_key(master_password):
-    """
-    Deriva una chiave Fernet dalla master password.
-    """
-    digest=hashlib.sha256(master_password.encode()).digest()
-    key=base64.urlsafe_b64encode(digest)
+def derive_key(masterpassword):
+    masterpassword_bytes = masterpassword.encode()
+    hashed_password = hashlib.sha256(masterpassword_bytes)
+    digest_bytes = hashed_password.digest()
+    key = base64.urlsafe_b64encode(digest_bytes)
     return key
 
-def encrypt_password(password, master_password):
-    """
-    Cifra una password in chiaro con la master password.
-    """
-    key = derive_key(master_password)
-    f = Fernet(key)
-    return f.encrypt(password.encode()).decode()
+def encrypt_password(passwords, masterpasswords):
+    key = derive_key(masterpasswords)
+    fernet_cipher = Fernet(key)
+    password_bytes = passwords.encode()
+    encrypted_bytes = fernet_cipher.encrypt(password_bytes)
+    encrypted_string = encrypted_bytes.decode()
+    return encrypted_string
 
-def decrypt_password(token, master_password):
-    """
-    Decifra una password cifrata con la master password.
-    """
-    key = derive_key(master_password)
-    f = Fernet(key)
-    return f.decrypt(token.encode()).decode()
+def decrypt_password(token, masterpassword):
+    key = derive_key(masterpassword)
+    fernet_cipher = Fernet(key)
+    token_bytes = token.encode()
+    decrypted_bytes = fernet_cipher.decrypt(token_bytes)
+    decrypted_string = decrypted_bytes.decode()
+    return decrypted_string
